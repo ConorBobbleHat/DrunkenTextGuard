c76050ea11d21f009ff5810bb4ea57cfc326a63d
Your Name <you@example.com>
2017-02-18 17:03:07 +0000

    Uncomment

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import twilio.twiml
 
 @route('/')
 def text():
-    #body = request.query["Body"]
-    #words = body.split(" ")
+    body = request.query["Body"]
+    words = body.split(" ")
 
-    #num = words.pop(0)
-    #body = " ".join(words)
+    num = words.pop(0)
+    body = " ".join(words)
     resp = twilio.twiml.Response()
-    resp.message("Hello")
+    resp.message(num)
     return str(resp)
 
 run(host='127.0.0.1',port=8080)
