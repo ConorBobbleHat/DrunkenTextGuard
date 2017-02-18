3f1297d9afa6973eba8e496a5d7836dbb404f5b7
Your Name <you@example.com>
2017-02-18 14:46:32 +0000

    Start gadget

--- gmail/GmailApp.py
+++ gmail/GmailApp.py
@@ -19,4 +19,18 @@ def signin():
     dire = context_io.post_connect_token(callback_url="http://localhost:8080/redir",email=request.query.email)
     redirect(dire["browser_redirect_url"])
 
+@route("/redir")
+def redir():
+    #Add webhook
+    account = c.ConnectToken(context_io,{"token":request.query.contextio_token})
+    account.get()
+    aid = account.account.id
+    account = account.account
+    # account.post_webhook(callback_url="http://localhost:8080/webhook",failure_notif_url="http://localhost:8080/webhook_failure")
+    account.post_webhook(callback_url="https://whitehat-a2boysandhats-1.c9users.io/webhook",failure_notif_url="https://whitehat-a2boysandhats-1.c9users.io/webhook_failure")
+
+@route("/webhook")
+def webhook():
+    return ""
+
 run(host="localhost", port=8080)
