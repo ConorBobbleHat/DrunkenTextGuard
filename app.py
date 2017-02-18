5854b36e0a7026a6c373a69310fb5ee1ef9e2e18
Your Name <you@example.com>
2017-02-18 18:23:56 +0000

    Fix merge

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import model
 
 t = model.TextBasedClassifier.TextBasedClassifier.TextBasedClassifier()
 df = t.TrainTextBasedClassifier()
+a = model.AylienApi.AylienEmotionClassifier()
 
 @route('/')
 def text():
@@ -14,7 +15,9 @@ def text():
     num = words.pop(0)
     body = " ".join(words)
     resp = twilio.twiml.Response()
-    resp.message(num)
+
+    emotion = a.getEmotion(body)
+    resp.message(str(t.Predict(body)["Prob_0"]) + str(emotion))
     return str(resp)
 
 run(host='0.0.0.0',port=8080)
