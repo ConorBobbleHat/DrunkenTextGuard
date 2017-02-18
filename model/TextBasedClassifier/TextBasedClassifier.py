22977779d9c5a22e68975c2a1c742e6c1cad6807
dmenin <dmenin@gmail.com>
2017-02-18 17:28:02 +0000

    ignoring MODEL file

--- model/TextBasedClassifier/TextBasedClassifier.py
+++ model/TextBasedClassifier/TextBasedClassifier.py
@@ -138,15 +138,7 @@ class TextBasedClassifier():
         dfpred = dfpred[self.MODEL_FEATURES]
         return pd.DataFrame(self.MODEL.predict_proba(dfpred) ,index = dfpred.index, columns = ['Prob_'+str(i) for i in self.MODEL.classes_])
 
-#
-#
-#t2=TextBasedClassifier()
-#df = t2.TrainTextBasedClassifier()
-#s = t2.Predict('I am drunk')
-#
-#s['Prob_0']
-#s['Prob_1']
-#    
+
 #    
 #    #############################TEST WITH TRAINING DATA (OVERFITING)
 #    
--- model/main.py
+++ model/main.py
@@ -1,5 +1,11 @@
+from TextBasedClassifier import TextBasedClassifier
 
 
 class Model():
+    t2=TextBasedClassifier()
+    df = t2.TrainTextBasedClassifier()
+    s = t2.Predict('I am drunk')
     
-    
+    s['Prob_0']
+    s['Prob_1']
+    #    
