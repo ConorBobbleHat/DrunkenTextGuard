f16b89bf950a40b0fdcb65939e1fd430a264cbc3
Henrique Zacchi <hzacchi@gmail.com>
2017-02-18 14:34:10 +0000

    returning full address and place attributes

--- model/LocationAnalysis/LocationAnalyzer.py
+++ model/LocationAnalysis/LocationAnalyzer.py
@@ -8,14 +8,33 @@ class LocationAnalyzer():
     def __init__(self):
         print "Class Initialized"
 
-    def HasBeenInABar(self, lat, long):
+    def VisitedPlaces(self, lat, lng):
+        placesVisited=[];
+
         google_places = GooglePlaces(API_KEY)
+        location = { 'lat': lat, 'lng': lng}
+
+        query_result = google_places.nearby_search(lat_lng=location, radius=100)
         
-        query_result = google_places.nearby_search(
-            location='London, England', keyword='Fish and Chips',
-            radius=20000, types=[types.TYPE_FOOD])
+        placesIds = [];
+        for place in query_result.places:
+            # Returned places from a query are place summaries.
+            placesIds.append(place.place_id)
+
+        for placeid in placesIds:
+            place_query_result = google_places.get_place(placeid)
+            
+            placeVisited = {
+                'formatted_address': place_query_result.formatted_address,
+                'place_types': place_query_result.types
+                };
+
+            placesVisited.append(placeVisited);
+
+        return placesVisited;
+
 
 lAnalyzer = LocationAnalyzer()
-lAnalyzer.HasBeenInABar(1, 2)
-
-
+results = lAnalyzer.VisitedPlaces(53.3452026, -6.2642727)
+for result in results:
+    print result,'\t'
